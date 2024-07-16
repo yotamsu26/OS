1152c4c70cca55de81539df68cffac8443190721
yotamsu <yotamsu@wix.com>
2024-07-16 20:13:40 +0300

    skeleton ready

--- ex4/VirtualMemory.cpp
+++ ex4/VirtualMemory.cpp
@@ -1,152 +1,125 @@
 #include "PhysicalMemory.h"
 
-uint64_t getOffSet(int depth, uint64_t virtualAddress){
-
-    uint64_t firstOffSetWidth = depth==0 ? 0:VIRTUAL_ADDRESS_WIDTH%OFFSET_WIDTH;
-    uint64_t otherOffSetWidth = depth==0 ? 0:OFFSET_WIDTH * (depth-1);
-
-    uint64_t moveTheLeftBits = virtualAddress << (otherOffSetWidth + firstOffSetWidth);
-    uint64_t backToCorrectPosition = moveTheLeftBits >> (otherOffSetWidth + firstOffSetWidth);
-    uint64_t moveBitsToTheRight = backToCorrectPosition >> (OFFSET_WIDTH * (TABLES_DEPTH - depth - 1));
-    return moveBitsToTheRight;
-}
-
-word_t  getNewDistance(word_t frame, word_t pageFrame){
-    word_t absFrame = (pageFrame-frame > 0)? pageFrame-frame : frame-pageFrame;
-    word_t num1 = word_t(NUM_PAGES) - absFrame;
-    word_t num2 = absFrame;
-    return (num1 > num2) ? num2: num1;
-}
-
-void expand(uint64_t frame) {
-    for(uint64_t i = 0; i<PAGE_SIZE;i++) {
-        PMwrite(frame*PAGE_SIZE+i, 0);
+int calcInitialWidth() {
+    if (VIRTUAL_ADDRESS_WIDTH % OFFSET_WIDTH == 0) {
+        return OFFSET_WIDTH;
+    } else {
+        return VIRTUAL_ADDRESS_WIDTH % OFFSET_WIDTH;
     }
 }
 
-typedef struct FreeFrame{
-    word_t emptyTableFrame;
-    word_t unusedFrame;
-    word_t evictedFrame;
-    word_t maxDistanceFrame;
+uint64_t getOffSet(uint64_t i, uint64_t virutalAddress) {
+    int initialWidth = calcInitialWidth();
+    if (i == 0) {
+        return virutalAddress >> (VIRTUAL_ADDRESS_WIDTH - initialWidth);
+    } else {
+        virutalAddress <<= (initialWidth + OFFSET_WIDTH * (i - 1));
 
-    FreeFrame():
-        emptyTableFrame(0),
-        unusedFrame(0),
-        evictedFrame(0),
-        maxDistanceFrame(0){}
+        virutalAddress >>= (VIRTUAL_ADDRESS_WIDTH - OFFSET_WIDTH);
 
-    void updateMaxUnusedFrame(word_t frame){
-        if(frame > this->unusedFrame){
-            this->unusedFrame = frame;
-        }
+        virutalAddress &= (PAGE_SIZE - 1);
+
+        return virutalAddress;
     }
-
-    void updateMaxDistanceFrame(word_t frame, word_t pageFrame){
-        word_t newDist = getNewDistance(frame, pageFrame);
-        if(newDist > this->maxDistanceFrame){
-            this->maxDistanceFrame = newDist;
-            this->evictedFrame = frame;
-        }
-    }
-
-    bool disconnectFrame(uint64_t frame, uint64_t frameToDisconnect) const{
-        if(this->emptyTableFrame == frame){
-            PMwrite(frameToDisconnect, 0);
-            return true;
-        }
-        return false;
-    }
-
-    word_t evictFrame(bool needToExpand) const{
-        word_t addr1 = 0;
-        uint64_t pageTableFrame = 0;
-
-        for(word_t d=0; d<TABLES_DEPTH; d++){
-            uint64_t offset = getOffSet(d, this->evictedFrame);
-            pageTableFrame = addr1 * PAGE_SIZE + offset;
-
-            PMread(pageTableFrame, &addr1);
-        }
-
-        PMevict(addr1, this->evictedFrame);
-        if(needToExpand){
-            expand(addr1);
-        }
-
-        PMwrite(pageTableFrame, 0);
-
-        return addr1;
-    }
-
-} FreeFrame;
-
-word_t frameReturnFunc(FreeFrame* newFrame, word_t depth){
-
-    bool needToExpand = (depth != TABLES_DEPTH-1);
-
-    if(newFrame->emptyTableFrame != 0){
-        return newFrame->emptyTableFrame;
-    }
-
-    if(newFrame->unusedFrame + 1 < NUM_FRAMES){
-        if(needToExpand){
-            expand(newFrame->unusedFrame + 1);
-        }
-        return newFrame->unusedFrame + 1;
-    }
-
-    return newFrame->evictFrame(needToExpand);
 }
 
-void traverseTree(word_t baseFrame, word_t currRoute, word_t pageFrame,
-                  FreeFrame* newFrame, word_t depth, bool isOriginFrame){
-
-    newFrame->updateMaxUnusedFrame(baseFrame);
-
-    if(depth == TABLES_DEPTH){
-        newFrame->updateMaxDistanceFrame(currRoute, pageFrame);
-        return;
+void eraseFrame(uint64_t frameInd) {
+    for (uint64_t i = 0; i < PAGE_SIZE; ++i) {
+        PMwrite(frameInd * PAGE_SIZE + i, 0);
     }
+}
 
-    for(int i = 0; i < PAGE_SIZE; i++){
+word_t pageDist(word_t firstPage, word_t secondPage) {
+    word_t dist1 = firstPage - secondPage < 0 ? secondPage - firstPage : firstPage - secondPage;
+    word_t dist2 = NUM_PAGES - dist1;
+    if (dist2 < dist1) {
+        return dist2;
+    }
+    return dist1;
+}
 
-        word_t checkNextFrame = 0;
-        auto frameToRead = baseFrame*PAGE_SIZE + i;
-        word_t updatedRoute = (currRoute << OFFSET_WIDTH) + i;
+void findMaxDistance(word_t currPage, word_t& maxPage, word_t wantedPage) {
+    if (pageDist(wantedPage, currPage) > pageDist(wantedPage, maxPage)) {
+        maxPage = currPage;
+    }
+}
 
-        PMread(frameToRead, &checkNextFrame);
+word_t treeTraversal(word_t baseFrame, word_t route, word_t depth, word_t& maxFrame,
+                     word_t& maxDistPage, word_t wantedPage, word_t prevFrame) {
+    if (maxFrame < baseFrame) {
+        maxFrame = baseFrame;
+    }
+    if (depth == TABLES_DEPTH) {
+        findMaxDistance(route, maxDistPage, wantedPage);
+        return 0;
+    }
+    bool routeExists = false;
+    word_t i = 0;
+    while (i < PAGE_SIZE) {
+        word_t nextFrame = 0;
+        auto frameToRead = baseFrame * PAGE_SIZE + i;
 
-        if(checkNextFrame != 0){
-            traverseTree(checkNextFrame, updatedRoute, pageFrame, newFrame, depth+1, false);
-            if(newFrame->disconnectFrame(checkNextFrame, frameToRead)){
-                return;
+        PMread(frameToRead, &nextFrame);
+        if (nextFrame != 0) {
+            word_t foundFrame = treeTraversal(nextFrame, (route << OFFSET_WIDTH) + i,
+                                              depth + 1, maxFrame, maxDistPage,
+                                              wantedPage, prevFrame);
+            if (foundFrame != 0) {
+                if (nextFrame == foundFrame) {
+                    PMwrite(baseFrame * PAGE_SIZE + i, 0); // disconnect curr from next
+                }
+                return foundFrame;
             }
+            routeExists = true;
         }
+        i++;
     }
-
-    if(isOriginFrame){
-        newFrame->emptyTableFrame = baseFrame;
+    if (!routeExists && (baseFrame != prevFrame)) {
+        return baseFrame;
     }
+    return 0;
 }
 
-word_t getAddrForFrame(word_t pagePath, word_t depth, bool isOriginFrame){
-    auto* newFrame = new FreeFrame();
-    word_t startingFrame = 0;
-    word_t startingRoute = 0;
-
-    traverseTree(startingFrame, startingRoute, pagePath, newFrame, depth, isOriginFrame);
-
-    return frameReturnFunc(newFrame, depth);
+word_t evict(uint64_t pageInd, bool isFinalLevel) {
+    word_t retAddr = 0;
+    uint64_t frame = 0;
+    uint64_t offset = 0;
+    for (word_t i = 0; i < TABLES_DEPTH; i++) {
+        frame = retAddr * PAGE_SIZE;
+        offset = getOffSet(i, pageInd << OFFSET_WIDTH);
+        PMread(frame + offset, &retAddr);
+    }
+    PMevict(retAddr, pageInd);
+    if (!isFinalLevel) {
+        eraseFrame(retAddr);
+    }
+    PMwrite(frame + offset, 0);
+    return retAddr;
 }
 
-void readLeaf(uint64_t addr1, uint64_t virtualAddress, word_t* value){
+word_t findFreeAddr(word_t queriedPageIndex, word_t originFrame, bool isFinalLevel) {
+    word_t maxFrameIndex = 0;
+    word_t maxDistPageIndex = queriedPageIndex;
+    word_t freeFrameIndex = treeTraversal(0, 0, 0,
+                                          maxFrameIndex, maxDistPageIndex, queriedPageIndex, originFrame);
+    if (freeFrameIndex != 0) {
+        return freeFrameIndex;
+    } else if (maxFrameIndex < NUM_FRAMES - 1) {
+        if (!isFinalLevel) {
+            eraseFrame(maxFrameIndex + 1);
+        }
+        return maxFrameIndex + 1;
+    }
+    return evict(maxDistPageIndex, isFinalLevel);
+}
+
+void readLeaf(uint64_t addr1, uint64_t virtualAddress, word_t *value) {
     uint64_t offset = getOffSet(TABLES_DEPTH, virtualAddress);
     uint64_t pageFrame = addr1 * PAGE_SIZE + offset;
     PMread(pageFrame, value);
 }
 
-void writeLeaf(uint64_t addr1, uint64_t virtualAddress, word_t value){
+void writeLeaf(uint64_t addr1, uint64_t virtualAddress, word_t value) {
     uint64_t offset = getOffSet(TABLES_DEPTH, virtualAddress);
     uint64_t pageFrame = addr1 * PAGE_SIZE + offset;
     PMwrite(pageFrame, value);
@@ -155,8 +128,8 @@ void writeLeaf(uint64_t addr1, uint64_t virtualAddress, word_t value){
 /*
  * Initialize the virtual memory.
  */
-void VMinitialize(){
-    for(int i = 0; i < PAGE_SIZE; i++){
+void VMinitialize() {
+    for (int i = 0; i < PAGE_SIZE; i++) {
         PMwrite(i, 0);
     }
 }
@@ -169,38 +142,38 @@ void VMinitialize(){
  * returns 0 on failure (if the address cannot be mapped to a physical
  * address for any reason)
  */
-int VMread(uint64_t virtualAddress, word_t* value){
-    if(virtualAddress > VIRTUAL_ADDRESS_WIDTH){
+int VMread(uint64_t virtualAddress, word_t *value) {
+    if (virtualAddress >= VIRTUAL_MEMORY_SIZE) {
         return 0;
     }
 
     word_t addr1 = 0;
     word_t pageTableFrame = 0;
-    word_t virtualAddressWithoutOffset = word_t(virtualAddress >> OFFSET_WIDTH);
+    word_t prevFrame = 0;
     bool needToRestore = false;
 
-    for(int d = 0; d < TABLES_DEPTH; d++){
+    for (int d = 0; d < TABLES_DEPTH; d++) {
         uint64_t offset = getOffSet(d, virtualAddress);
         pageTableFrame = addr1 * PAGE_SIZE + offset;
-
+        prevFrame = addr1;
         PMread(pageTableFrame, &addr1);
 
-        if(addr1 == 0){
-            addr1 = getAddrForFrame(virtualAddressWithoutOffset, d, true);
-
-            PMwrite(pageTableFrame, addr1);
+        if (addr1 == 0) {
+            auto addr2 = word_t(findFreeAddr(word_t(virtualAddress >> OFFSET_WIDTH),
+                                             prevFrame, d == TABLES_DEPTH - 1));
+            PMwrite(pageTableFrame, addr2);
             needToRestore = true;
+            addr1 = addr2;
         }
     }
 
-    if(needToRestore){
-        PMrestore(addr1, virtualAddressWithoutOffset);
+    if (needToRestore) {
+        PMrestore(addr1, virtualAddress >> OFFSET_WIDTH);
     }
 
     readLeaf(addr1, virtualAddress, value);
     return 1;
 }
-
 
 /* Writes a word to the given virtual address.
  *
@@ -208,32 +181,31 @@ int VMread(uint64_t virtualAddress, word_t* value){
  * returns 0 on failure (if the address cannot be mapped to a physical
  * address for any reason)
  */
-int VMwrite(uint64_t virtualAddress, word_t value){
-    if(virtualAddress > VIRTUAL_ADDRESS_WIDTH){
+int VMwrite(uint64_t virtualAddress, word_t value) {
+    if (virtualAddress >= VIRTUAL_MEMORY_SIZE) {
         return 0;
     }
-
     word_t addr1 = 0;
-    uint64_t pageTableFrame = 0;
-    uint64_t virtualAddressWithoutOffset = virtualAddress >> OFFSET_WIDTH;
+    word_t prevFrame = 0;
     bool needToRestore = false;
-
-    for(int d = 0; d < TABLES_DEPTH; d++){
+    for (int d = 0; d < TABLES_DEPTH; d++) {
         uint64_t offset = getOffSet(d, virtualAddress);
-        pageTableFrame = addr1 * PAGE_SIZE + offset;
-
+        uint64_t pageTableFrame = addr1 * PAGE_SIZE + offset;
+        prevFrame = addr1;
         PMread(pageTableFrame, &addr1);
 
-        if(addr1 == 0){
-            addr1 = getAddrForFrame(virtualAddressWithoutOffset, d, true);
-
-            PMwrite(pageTableFrame, addr1);
+        if (addr1 == 0) {
+            auto newAddr = word_t(findFreeAddr(word_t(virtualAddress >> OFFSET_WIDTH),
+                                               prevFrame, d == TABLES_DEPTH - 1));
+            int addr2 = newAddr;
+            PMwrite(pageTableFrame, addr2);
             needToRestore = true;
+            addr1 = addr2;
         }
     }
 
-    if(needToRestore){
-        PMrestore(addr1, virtualAddressWithoutOffset);
+    if (needToRestore) {
+        PMrestore(addr1, virtualAddress >> OFFSET_WIDTH);
     }
 
     writeLeaf(addr1, virtualAddress, value);
